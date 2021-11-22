a25dab415b28e48530abcbb8cc6a6a54c67a8d11
mariaciornii <70654766+mariaciornii@users.noreply.github.com>
2021-11-22 09:16:29 +0200

    Add files via upload

---
+++ problema 16.py
@@ -0,0 +1,5 @@
+S1=str(input(‘ Întroduceti primul sir :’))
+S2=str(input(‘introduceti al doilea sir :’))
+Print(d’a ) caracterele care se intalnesc in unul dintre siruri: {set(S1)&set(S2)}’}
+Print(d ‘b) caracterele care apar in ambele șiruri : {set (S1) | set (S2)}’)
+Print (d’ c) caracterele care apar in primul șir și nu apar in al doilea :{ set (S1)-set(S1)}’)
